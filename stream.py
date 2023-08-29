b272e708d98988896d44e674d46e3d591259dd50
Charu Priya Singh <66252642+charusingh96@users.noreply.github.com>
2023-08-29 16:53:41 -0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -10,5 +10,5 @@ Original file is located at
 
 
 # Commented out IPython magic to ensure Python compatibility.
- st.write('#Waddup')
- st.write('##welcome')
+st.write('#Waddup')
+st.write('##welcome')
