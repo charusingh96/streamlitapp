e078e03d38a95b4545092c991133dc4be04872a6
Charu Priya Singh <66252642+charusingh96@users.noreply.github.com>
2023-08-29 18:13:48 -0700

    Add files via upload

---
+++ portfolio_app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+get_ipython().system('pip install streamlit')
+import streamlit as st
+
+def main():
+    st.title("My Portfolio")
+    st.write("Welcome to my portfolio!")
+
+    # Add your project descriptions, images, and links here
+
+if __name__ == "__main__":
+    main()
+
+
+# In[ ]:
+
+
+
+
